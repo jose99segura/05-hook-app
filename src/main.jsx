c6b076a2add7c85c38ec2e836d6cdf4ce8d3cd59
jose99segura <jose99segura@gmail.com>
2023-02-07 11:29:37 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { CounterApp } from './01-useState/CounterApp'
+// import { HooksApp } from './HooksApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CounterWithCustomHook />
+  </React.StrictMode>,
+)
