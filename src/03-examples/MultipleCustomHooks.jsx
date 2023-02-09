63ea275b9dbe608a4398ba7a72a6bfd182a46979
jose99segura <jose99segura@gmail.com>
2023-02-09 11:22:50 +0100

    Formulario custom Hook

---
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,12 @@
+
+
+export const MultipleCustomHooks = () => {
+  return (
+    <>
+
+    <h1>Breaking bad Quotes</h1>
+    <hr />
+
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { SimpleForm } from './02-useEffect/simpleForm'
 
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { SimpleForm } from './02-useEffect/simpleForm'
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { HooksApp } from './HooksApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SimpleForm />
+    <MultipleCustomHooks />
   </React.StrictMode>,
 )
