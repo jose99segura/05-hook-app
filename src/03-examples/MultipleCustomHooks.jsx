7cfe279472e6253dbfadf64b19ce37a733fab99a
jose99segura <jose99segura@gmail.com>
2023-03-06 17:40:23 +0100

    Continuando custom hooks

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,11 +1,45 @@
+import { useFetch } from "../hooks/useFetch"
+import { useCounter } from "../hooks/useCounter"
 
 
 export const MultipleCustomHooks = () => {
+
+  const {counter, increment} = useCounter(1);
+
+  const { data, isLoading, hasError } = useFetch(`https://rickandmortyapi.com/api/character/${counter}`);
+
+  const { name, species } = !!data && data; //!!data convierte el valor de undefined a false
+
+  if (isLoading) {
+    return(
+      <h1>Cargando...</h1>
+    )
+  }
+
   return (
     <>
 
-    <h1>Breaking bad Quotes</h1>
+    <h1>Rick y Morty personajes</h1>
     <hr />
+
+    {
+      (isLoading) 
+      ?(
+        <div className="alert alert-info text-center">
+          Loading...
+        </div>
+      )
+      : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-1">{name}</p>
+          <footer className="blockquote-footer">{species}</footer>
+        </blockquote>
+      )
+    }
+
+    <button onClick={ () => increment() } className="btn btn-primary">
+      Proximo personaje
+    </button>
 
     </>
   )
